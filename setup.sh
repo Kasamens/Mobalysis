c43cc8b877d9d423cc439d5d6d6d382a36dc11f5
Kojo Asamoa-Mensah <kwadwoasamoa7@gmail.com>
2022-02-14 13:59:02 +0000

    Modified setup.sh to Create mobalytics database and set owner to mob_db_user

--- setup.sh
+++ setup.sh
@@ -2,11 +2,16 @@
 
 apt-get update
 
-apt-get install postgresql postgresql-contrib
+apt-get install postgresql postgresql-contrib -y
 
-apt-get install python3 venv
+sudo service postgresql start
+
+apt-get install python3-venv -y
 
 sudo -u postgres createuser 'mob_db_user'
 
 sudo -u postgres psql -c "ALTER ROLE mob_db_user WITH password 'mob_db_pass'"
 
+sudo -u postgres createdb mobalytics
+
+sudo -u postgres psql -c "ALTER DATABASE mobalytics OWNER TO mob_db_user"
