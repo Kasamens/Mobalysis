a965bbd8cd2a44e5e078809cb94a346c26beaa0c
Kojo Asamoa-Mensah <kwadwoasamoa7@gmail.com>
2022-02-14 13:21:30 +0000

    Modified setup.sh to install python3-venv

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@ apt-get update
 
 apt-get install postgresql postgresql-contrib
 
+apt-get install python3 venv
+
 sudo -u postgres createuser 'mob_db_user'
 
 sudo -u postgres psql -c "ALTER ROLE mob_db_user WITH password 'mob_db_pass'"
+
