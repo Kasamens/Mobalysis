7122379b4ad3e1675ada5782889472f0418cd7b3
sheilabett <sheilahbett@gmail.com>
2022-02-14 14:26:26 -0500

    I added script command to create system user: mob_app_usr

--- setup.sh
+++ setup.sh
@@ -15,3 +15,10 @@ sudo -u postgres psql -c "ALTER ROLE mob_db_user WITH password 'mob_db_pass'"
 sudo -u postgres createdb mobalytics
 
 sudo -u postgres psql -c "ALTER DATABASE mobalytics OWNER TO mob_db_user"
+
+echo "creating new system user with name mob_app_usr"
+sys_user="mob_app_usr"
+user_dir=/home/$sys_user
+
+sudo useradd -m -d $user_dir $sys_user
+
